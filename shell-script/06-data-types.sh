2f190a33be8badf579112af883f61e48975c3e13
Srinivas355 <ksrinivas9736@gmail.com>
2024-12-04 21:05:22 +0530

    shell-script-variables06

--- shell-script/06-data-types.sh
+++ shell-script/06-data-types.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-int NUMBER1=$1
-int NUMBER2=$2
+NUMBER1=$1
+NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo Total:: $SUM
+echo "Total:: $SUM"
+
+echo "How many args passed:: $#"
+
+echo "All args passed:: $@"
+
+echo "Script name:: $0"
