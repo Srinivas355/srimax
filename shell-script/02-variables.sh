836b56a25587d13eca0346ab42bf81e0e193d749
Srinivas355 <ksrinivas9736@gmail.com>
2024-12-04 19:32:36 +0530

    shell-script-sample

---
+++ shell-script/02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "srinivas: Hello siva, Good Morning"
+echo "siva    : Hi srinivas,very good morning"
+echo "srinivas: How are you siva?"
+echo "siva    : I am good,what about you srinivas?"
