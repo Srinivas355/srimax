88d7db1b11ee992c4dbe22d1557d18bd9710452d
Srinivas355 <ksrinivas9736@gmail.com>
2024-12-04 19:41:06 +0530

    shell-script-variables1

--- shell-script/02-variables.sh
+++ shell-script/02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1 = srinivas
 PERSON2 = siva
 
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1,very good morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good,what about you $PERSON1?"
+echo $PERSON1: Hello $PERSON2, Good Morning
+echo $PERSON2: Hi $PERSON1,very good morning
+echo $PERSON1: How are you $PERSON2?
+echo $PERSON2: I am good,what about you $PERSON1?
