88958ad13ea59e489f756df593bd52900bb4a92e
Srinivas355 <ksrinivas9736@gmail.com>
2024-12-09 20:55:29 +0530

    11-logs.sh

--- shell-script/11-logs.sh
+++ shell-script/11-logs.sh
@@ -4,13 +4,15 @@ ID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 echo "script name: $0"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo "ERROR:: $2....FAILED"
-        exit1
+        exit 1
     else
         echo "$2....SUCCESS"
     fi 
@@ -19,7 +21,7 @@ VALIDATE(){
 if [ $ID -ne 0 ]
 then
    echo "ERROR:: Please run this script with root access"
-   exit1
+   exit 1
 else
   echo "you are root user"
 fi
